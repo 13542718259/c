6dfea678337c4e0758813eccfb8a6f5c90070dff
执梗 <84454978+13542718259@users.noreply.github.com>
2021-05-24 19:20:11 +0800

    Add files via upload

---
+++ test5-24_1.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(void)
+{
+	int i;
+	int ans;
+	srand(time(NULL));
+	ans = rand() % 7;
+	puts("请输入0~6的数");
+	scanf_s("%d",&i);
+	switch (i)
+	{
+	case 0:
+		puts("\a大吉！");
+		break;
+	case 1:
+		puts("\a中吉！");
+		break;
+	case 2:
+		puts("\a小吉！");
+		break;
+	case 3:
+		puts("\a吉！");
+		break;
+	case 4:
+		puts("\a末凶！");
+		break;
+	case 5:
+		puts("\a凶！");
+		break;
+	case 6:
+		puts("\a大吉！");
+		break;
+	default:
+		break;
+	}
+	return 0;
+}
